ed2697c8f7111d8601782ec64bd27eb5e889238d
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-03-31 21:36:43 -0700

    Added a basic number guessing game; I am learning rust for the first time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    number_guesser();
+}
+fn number_guesser() {
+    println!("Guess a number between 1 and 10");
+    println!("Please input your guess.");
+    let mut answer = 3;
+    let mut guess = String::new();
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    if guess.trim() == answer.to_string() {
+        println!("You guessed the right number!");
+    }/* elseif guess.trim >= 10 {
+        println!("Sorry, that's not the right number.");
+    }*/
 }
