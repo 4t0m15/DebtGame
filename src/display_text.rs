f542948af504a8c26d006334239b9ce4e0c5d6eb
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-01 21:04:09 -0700

    Add display_text module with story initialization; using Rexpaint for image making, planning on having stories

---
+++ src/display_text.rs
@@ -0,0 +1,27 @@
+pub fn init_story() {
+    println!("                         
+                         
+         ♦               
+       ♦♦ ♦       ♦♦♦    
+      ♦    ♦    ♦♦   ♦♦  
+     ♦      ♦   ♦     ♦  
+   ♦♦     ♦♦   ♦  ♦ ♦  ♦ 
+  ♦      ♦     ♦       ♦ 
+ ♦      ♦♦     ♦ ♦   ♦ ♦ 
+  ♦♦  ♦♦ ♦      ♦ ♦♦♦ ♦  
+    ♦♦   ♦      ♦♦   ♦♦  
+         ♦♦       ♦♦♦    
+          ♦        ♦     
+          ♦        ♦     
+          ♦♦♦♦♦♦   ♦     
+                ♦♦♦♦     
+                   ♦     
+                   ♦     
+                   ♦     
+                  ♦♦♦    
+                 ♦♦ ♦♦   
+                 ♦   ♦   
+                         ");
+    println!("'Hello, You are in Debt of a MILLION BUCKS!!!'");
+    println!(" - THE MOB");
+}
