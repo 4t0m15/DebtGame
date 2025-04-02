489718157817e5e50ff7423dbd3d04eeb5204c03
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-01 22:13:20 -0700

    Fixed file history

--- src/display_text.rs
+++ src/display_text.rs
@@ -1,36 +1,9 @@
-pub fn init_story() {
-//     println!("                         
-//                          
-//          ♦               
-//        ♦♦ ♦       ♦♦♦    
-//       ♦    ♦    ♦♦   ♦♦  
-//      ♦      ♦   ♦     ♦  
-//    ♦♦     ♦♦   ♦  ♦ ♦  ♦ 
-//   ♦      ♦     ♦       ♦ 
-//  ♦      ♦♦     ♦ ♦   ♦ ♦ 
-//   ♦♦  ♦♦ ♦      ♦ ♦♦♦ ♦  
-//     ♦♦   ♦      ♦♦   ♦♦  
-//          ♦♦       ♦♦♦    
-//           ♦        ♦     
-//           ♦        ♦     
-//           ♦♦♦♦♦♦   ♦     
-//                 ♦♦♦♦     
-//                    ♦     
-//                    ♦     
-//                    ♦     
-//                   ♦♦♦    
-//                  ♦♦ ♦♦   
-//                  ♦   ♦   
-//                          ");
-//     println!("'Hello, You are in Debt of a MILLION BUCKS!!!'");
-//     println!(" - THE MOB");
-    use std::fs;
-    use std::io;
-    pub fn init_story() -> io::Result<()> {
-        let file_path = "text/example.txt";
-        println!("In file {file_path}");
-        let contents = fs::read_to_string(file_path);
-        //println!("With text:\n{contents}");
-        Ok(())
-    };
+use std::fs;
+use std::io;
+pub fn init_story() -> io::Result<()> {
+    let file_path = "text/example.txt";
+    //println!("In file {file_path}");
+    let contents = fs::read_to_string(file_path)?; //contents would be a result enum (Result<String>) using ? to contents.unwrap()
+    println!("With text:\n{}", contents);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
 mod display_text;
-
 fn main() {
-    //println!("Hello, world!");
-    //number_guesser();
-    //display_text::display_text();
     display_text::init_story();
 }
-// fn number_guesser() {
-//     println!("Guess a number between 1 and 10");
-//     println!("Please input your guess.");
-//     let mut answer = 3;
-//     let mut guess = String::new();
-//     std::io::stdin()
-//         .read_line(&mut guess)
-//         .expect("Failed to read line");
-//     if guess.trim() == answer.to_string() {
-//         println!("You guessed the right number!");
-//     }/* elseif guess.trim >= 10 {
-//         println!("Sorry, that's not the right number.");
-//     }*/
-// }
