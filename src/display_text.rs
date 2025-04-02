832960e7de6be2411d5cbabf007dff8281a8a077
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-01 21:22:13 -0700

    Refactor init_story function to read from a file and print its contents, but it doesn't work lol

--- src/display_text.rs
+++ src/display_text.rs
@@ -1,27 +1,36 @@
 pub fn init_story() {
-    println!("                         
-                         
-         ♦               
-       ♦♦ ♦       ♦♦♦    
-      ♦    ♦    ♦♦   ♦♦  
-     ♦      ♦   ♦     ♦  
-   ♦♦     ♦♦   ♦  ♦ ♦  ♦ 
-  ♦      ♦     ♦       ♦ 
- ♦      ♦♦     ♦ ♦   ♦ ♦ 
-  ♦♦  ♦♦ ♦      ♦ ♦♦♦ ♦  
-    ♦♦   ♦      ♦♦   ♦♦  
-         ♦♦       ♦♦♦    
-          ♦        ♦     
-          ♦        ♦     
-          ♦♦♦♦♦♦   ♦     
-                ♦♦♦♦     
-                   ♦     
-                   ♦     
-                   ♦     
-                  ♦♦♦    
-                 ♦♦ ♦♦   
-                 ♦   ♦   
-                         ");
-    println!("'Hello, You are in Debt of a MILLION BUCKS!!!'");
-    println!(" - THE MOB");
+//     println!("                         
+//                          
+//          ♦               
+//        ♦♦ ♦       ♦♦♦    
+//       ♦    ♦    ♦♦   ♦♦  
+//      ♦      ♦   ♦     ♦  
+//    ♦♦     ♦♦   ♦  ♦ ♦  ♦ 
+//   ♦      ♦     ♦       ♦ 
+//  ♦      ♦♦     ♦ ♦   ♦ ♦ 
+//   ♦♦  ♦♦ ♦      ♦ ♦♦♦ ♦  
+//     ♦♦   ♦      ♦♦   ♦♦  
+//          ♦♦       ♦♦♦    
+//           ♦        ♦     
+//           ♦        ♦     
+//           ♦♦♦♦♦♦   ♦     
+//                 ♦♦♦♦     
+//                    ♦     
+//                    ♦     
+//                    ♦     
+//                   ♦♦♦    
+//                  ♦♦ ♦♦   
+//                  ♦   ♦   
+//                          ");
+//     println!("'Hello, You are in Debt of a MILLION BUCKS!!!'");
+//     println!(" - THE MOB");
+    use std::fs;
+    use std::io;
+    pub fn init_story() -> io::Result<()> {
+        let file_path = "text/example.txt";
+        println!("In file {file_path}");
+        let contents = fs::read_to_string(file_path);
+        println!("With text:\n{contents}");
+        Ok(())
+    };
 }
