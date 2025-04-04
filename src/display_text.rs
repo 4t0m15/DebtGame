57ff1c68046088591d154c164a5f4574b2fd7360
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-03 20:07:31 -0700

    Added example text of a guy holding money

--- src/display_text.rs
+++ src/display_text.rs
@@ -2,8 +2,7 @@ use std::fs;
 use std::io;
 pub fn init_story() -> io::Result<()> {
     let file_path = "text/example.txt";
-    //println!("In file {file_path}");
     let contents = fs::read_to_string(file_path)?; //contents would be a result enum (Result<String>) using ? to contents.unwrap()
-    println!("With text:\n{}", contents);
+    println!("GAME START!\n{}", contents);
     Ok(())
 }
