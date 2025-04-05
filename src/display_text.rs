90d8dd56ea40b963f97d9be20bd2375d38b136af
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-04 21:01:36 -0700

    added files for some organisation between the minigames and the colored library so the example text is now green. Added welcome message and changed example text.

--- src/display_text.rs
+++ src/display_text.rs
@@ -1,8 +1,15 @@
 use std::fs;
 use std::io;
+use colored::*;
 pub fn init_story() -> io::Result<()> {
     let file_path = "text/example.txt";
     let contents = fs::read_to_string(file_path)?; //contents would be a result enum (Result<String>) using ? to contents.unwrap()
-    println!("GAME START!\n{}", contents);
+    println!("You are in Debt of a Million bucks and you need to figure something out.");
+    println!("GAME START!\n{}", contents.truecolor(39, 128, 34));//the {} is super cool why isnt this in other languages?
     Ok(())
+}
+pub fn init_options() {
+    // I love having one really long print statement with \n's
+    //TODO add help option and a menu with about and start game options
+    println!("Hello, you have 3 options when it comes to making the money \n 1 - Gambler \n 2 - Daytrader \n 3 - Dealer");
 }
