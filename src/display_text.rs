582125b2be55e11e5e7eeb71b0542b3362a4145a
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-07 21:48:36 -0700

    refactor input handling in display_text.rs for improved user interaction

--- src/display_text.rs
+++ src/display_text.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use std::io::prelude::*;
 use colored::*;
 pub fn init_story() -> io::Result<()> {
     let file_path = "text/example.txt";
@@ -12,21 +13,23 @@ pub fn init_options() {
     // I love having one really long print statement with \n's
     //TODO add help option and a menu with about and start game options
     println!("Hello, you have 3 options \n 1 - Gambler \n 2 - Daytrader \n 3 - Dealer");
-    /*
-    Scanner input = new Scanner(System.in);
-        printSplashScreen();
-        delay();
-        while (true) {
-            System.out.print("Choose an option: (a)bout, (n)ew game, (h)elp: ");
-            char choice = input.next().charAt(0);
-            if (choice == 'a') {
-                printAbout();
-            } else if (choice == 'n') {
-                startNewGame(input);
-            } else if (choice == 'h') {
-                printHelp();
-            } else {
-                System.out.println("Invalid option. Please choose again.");
-            }
-        }*/
+
+    let mut stdin = io::stdin();
+    let choice = &mut String::new();
+
+    loop {
+        choice.clear();
+        stdin.read_line(choice);
+        println!("{}", choice);
+    }
+/*    println!("Choose an option: (a)bout, (n)ew game, (h)elp: ");
+    if (choice == 'a') {
+        //printAbout();
+    } else if (choice == 'n') {
+        //starrsutNewGame(input);
+    } else if (choice == 'h') {
+        //printHelp();
+    } else {
+        System.out.println("Invalid option. Please choose again.");
+    }*/
 }
