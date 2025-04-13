eb882fd8ba6aa6d2f3b6192c01642ee2a34ff742
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-04-12 21:36:52 -0700

    simplify error message in display_text.rs

--- src/display_text.rs
+++ src/display_text.rs
@@ -28,6 +28,6 @@ pub fn init_options() {
     } else if (choice == 'n') {
     } else if (choice == 'h') {
     } else {
-        System.out.println("Invalid option. Please choose again.");
+        System.out.println("Invalid option.");
     }*/
 }
